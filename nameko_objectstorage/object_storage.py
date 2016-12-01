8aed997e4279f1440c20d09d4ff59672767612ef
Johan Frisell <johan.henric.frisell@gmail.com>
2016-12-01 21:30:55 +0100

    fixed some random stuff after merge

--- nameko_objectstorage/object_storage.py
+++ nameko_objectstorage/object_storage.py
@@ -1,7 +1,10 @@
+import io
+from StringIO import StringIO
+
 import requests
 from requests.auth import AuthBase
+
 from nameko.extensions import DependencyProvider
-from StringIO import StringIO
 
 
 class ObjectStorage(DependencyProvider):
@@ -36,40 +39,45 @@ class Connection(object):
 
     def get_binary(self, container, object_name):
         url = self._construct_object_url(container, object_name)
-        r = requests.get(url, auth=self.auth)
-        
-        return io.BytesIO(r.content)
+        r = requests.get(url, auth=self.auth, stream=True)
+        output = io.BytesIO()
+
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                output.write(chunk)
+
+        output.seek(0)
+
+        return output
 
     def get_object_metadata(self, container, object_name):
         url = self._construct_object_url(container, object_name)
         r = requests.head(url, auth=self.auth)
-        
+
         return r.headers
 
     def delete_object(self, container, object_name):
         url = self._construct_object_url(container, object_name)
         r = requests.delete(url, auth=self.auth)
+        return r
 
     def list_objects(self, container):
-        url = self._construct_list_objects_url(container)
+        url = self._construct_container_url(container)
         r = requests.get(url, auth=self.auth)
-        
+
         return [line.decode('utf-8') for line in r.iter_lines()]
 
-    def _construct_list_objects_url(self, container):
+    def _construct_container_url(self, container):
         return 'https://{0}/{1}/AUTH_{2}/{3}'.format(
             self.config['access_point'],
             self.config['api_version'],
             self.config['project_id'],
             container
         )
-        
+
     def _construct_object_url(self, container, object_name):
-        return 'https://{0}/{1}/AUTH_{2}/{3}/{4}'.format(
-            self.config['access_point'],
-            self.config['api_version'],
-            self.config['project_id'],
-            container,
+        return 'https://{0}/{1}'.format(
+            self._construct_container_url(container),
             object_name
         )
 
